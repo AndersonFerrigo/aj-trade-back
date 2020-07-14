5ff9cd87a76ee682e1cf6e8ff7526b09f3dfe812
AndersonFerrigo <andersonferrigosilva@yahoo.com.br>
2020-07-14 19:48:43 -0300

    clearDB ready

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     HOST: "us-cdbr-east-02.cleardb.com",
-    USER: "b0b88dac63-eb3a",
+    USER: "b0b88dac63eb3a",
     PASSWORD: "24e4552a",
-    DB: "heroku_229ca7137a2c921"
+    DB: "heroku_229ca7137a2c921",
+    dialect: "mysql"
   };
