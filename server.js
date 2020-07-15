105221c7ab2e46b98c5caf18d5b2399a983fcb73
AndersonFerrigo <andersonferrigosilva@yahoo.com.br>
2020-07-15 12:46:50 -0300

    Change port to connect wit heroku

--- server.js
+++ server.js
@@ -36,6 +36,15 @@ require("./app/routes/pacotes.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running in port ${PORT}.`);
 });
