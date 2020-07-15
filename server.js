7ca28c9a59a4d9c9135d034161a0df72434a0a44
AndersonFerrigo <andersonferrigosilva@yahoo.com.br>
2020-07-15 12:59:19 -0300

    Change port to connect wit heroku

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-    origin: "http://localhost:5000" 
-};
 
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: (origin, callback) => callback(null, true),
+    credentials: true
+  })
+);
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
